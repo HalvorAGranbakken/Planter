6f90276945aab6601e77193bc975e424ab854164
HalvorAGranbakken <halvorag@gmail.com>
2025-10-30 11:16:10 +0100

    Delete PlanteSide directory

--- PlanteSide/resultLogic.js
+++
@@ -1,9 +0,0 @@
-const quizScore = sessionStorage.getItem("quizScore");
-const totalQuestions = sessionStorage.getItem("totalQuestions");
-
-window.onload = function() {
-    showScore();
-};
-
-function showScore(){
-    document.getElementById('score').textContent = `${quizScore} / ${totalQuestions}`;}
