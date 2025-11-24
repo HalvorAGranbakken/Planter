871b85e73fb481509806e340a150eb58477dc2bd
Halvor Allum Granbakken <halvorag@gmail.com>
2025-11-24 15:21:41 +0100

    hjem = index

--- PlanteSide/hamMenu.js
+++
@@ -1,8 +0,0 @@
-//hamburger menu
-const hamMenu = document.querySelector('.ham_menu');
-const offScreenMenu = document.querySelector('.off_screen_menu');
-
-hamMenu.addEventListener('click',()=>{
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-})
